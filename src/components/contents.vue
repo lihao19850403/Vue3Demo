c6d5a542410da51915df28652a14d1c91c43e8c8
li.hao85 <lihao_19850403@126.com>
2024-04-20 10:19:45 +0800

    Update contents page.

--- src/components/contents.vue
+++ src/components/contents.vue
@@ -1,12 +1,11 @@
 <template>
 	<h2>目录</h2>
-	<ul style="text-align: left;" v-for="(part, index) in contents" :key="part.path">
-		<span style="font-weight: bold;">第{{index+1}}部分：{{part.name}}</span>
-		<li style="list-style-type: none;" v-for="(chapter, index) in part.chapters" :key="chapter.path">
-			<button style="font-size: 13px;margin-bottom: 3px;" @click="nav(chapter.path)">第{{index+1}}节：{{chapter.name}}</button>
-		</li>
-		<br />
-	</ul>
+	<dl v-for="(part, index) in contents" :key="part.path">
+		<dt>第{{index+1}}部分：{{part.name}}</dt>
+		<dd v-for="(chapter, index) in part.chapters" :key="chapter.path">
+			<button @click="nav(chapter.path)">第{{index+1}}节：{{chapter.name}}</button>
+		</dd>
+	</dl>
 </template>
 
 <script>
@@ -44,5 +43,19 @@ export default {
 	text-align: center;
 	color: #2c3e50;
 	margin-top: 60px;
+}
+dl {
+	text-align: left;
+}
+dl dt {
+	font-weight: bold;
+}
+dl dd {
+	list-style-type: none; 
+	margin-left: 0;
+}
+dl dd button {
+	font-size: 13px;
+	margin-bottom: 3px;
 }
 </style>
